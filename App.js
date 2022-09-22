3b6a8b72a38ea7f563024206d98d87f43d44430c
LAISE OLIVEIRA SILVA <21276363@dsn127m1247444.cfp127aluno.local>
2022-09-22 16:55:44 -0300

    starting register

---
+++ App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+
+import {StyleSheet, SafeAreaView, View, Text, TextInput} from "react-native"
+
+
+const App = () => {
+  return (
+    <Text> Nome Paciente: </Text>
+  );
+};
+
+
+
+export default App;
---
+++ src/components/input.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {StyleSheet, SafeAreaView, View, Text, TextInput} from "react-native"
+
+const Input = () => {
+    ( 
+        <SafeAreaView>
+            <Text> Nome Paciente: </Text>
+
+            <View style={styles.corTeste}>
+                <View style={styles.teste}>
+                    <TextInput style={styles.input}/>
+                </View>
+            </View>
+        </SafeAreaView>
+      )
+
+
+      const styles = StyleSheet.create({
+        corTeste:{
+        backgroundColor:'#CCCCCC'
+      },
+    
+      title: {
+        fontSize: 24,
+        fontWeight: '600',
+      },
+    
+      teste:{
+        width: "100%",
+        height: 40,
+        backgroundColor: "#44C4C4"
+      },
+    
+      input:{
+        color: "#000000",
+       flex: 1
+      }
+    });
+
+}
+
+export default Input;;
